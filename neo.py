b645eb1fa2d173019aa951d8251ef5b14946b821
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-08-18 21:09:49 -0700

    Update variable names for readability

--- neo.py
+++ neo.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 
 
-def get_data(BASE_URL, API_KEY, start_date, end_date):
+def get_api_data(BASE_URL, API_KEY, start_date, end_date):
     """Get NASA API Data for Near Earth Object Asteroids Tracking"""
 
     API = get(f"{BASE_URL}start_date={start_date}&end_date={end_date}&api_key={API_KEY}");
@@ -14,39 +14,39 @@ def get_data(BASE_URL, API_KEY, start_date, end_date):
     return None
 
 def struct(near_earth_object, dates, element_key, object_type):
-    """Add each Asteroid Data Structred into Dictionary"""
+    """Near Earth Object Data Structured into Dictionary"""
     
-    obj_close_approach_data     = near_earth_object[dates[element_key]][object_type]['close_approach_data'][0]
-    individual_asteroid_data    = get(near_earth_object[dates[element_key]][object_type]['links']['self']).json()
+    individual_asteroid_data        = get(near_earth_object[dates[element_key]][object_type]['links']['self']).json()
+    asteroid_close_approach_data    = near_earth_object[dates[element_key]][object_type]['close_approach_data'][0]
     asteroid        = near_earth_object[dates[element_key]][object_type]
     asteroid_info   = defaultdict(list)
 
     asteroid_info['id']                     = asteroid['id'];
     asteroid_info['name']                   = asteroid['name'];
     asteroid_info['hazardous']              = asteroid['is_potentially_hazardous_asteroid'];
-    asteroid_info['close_approach_date']    = obj_close_approach_data['close_approach_date_full'];
-    asteroid_info['current_orbiting_body']  = obj_close_approach_data['orbiting_body'];
+    asteroid_info['close_approach_date']    = asteroid_close_approach_data['close_approach_date_full'];
+    asteroid_info['current_orbiting_body']  = asteroid_close_approach_data['orbiting_body'];
     asteroid_info['diameter_meter']         = asteroid['estimated_diameter']['meters'];
     asteroid_info['diameter_feet']          = asteroid['estimated_diameter']['feet'];
-    asteroid_info['velocity_km']            = obj_close_approach_data['relative_velocity']['kilometers_per_hour'];
-    asteroid_info['velocity_miles']         = obj_close_approach_data['relative_velocity']['miles_per_hour'];
+    asteroid_info['velocity_km']            = asteroid_close_approach_data['relative_velocity']['kilometers_per_hour'];
+    asteroid_info['velocity_miles']         = asteroid_close_approach_data['relative_velocity']['miles_per_hour'];
     asteroid_info['orbiting_bodies']        = asteroid_orbiting_bodies(individual_asteroid_data);
-    asteroid_info['orbiting_class_type']    = asteroid_orbiting_type(individual_asteroid_data)['orbit_class_type'];
-    asteroid_info['orbiting_type_description']  = asteroid_orbiting_type(individual_asteroid_data)['orbit_class_description'];
-    asteroid_info['first_observation']          = asteroid_observation_dates(individual_asteroid_data)['first_observation_date'];
-    asteroid_info['last_observation']           = asteroid_observation_dates(individual_asteroid_data)['last_observation_date'];
+    asteroid_info['orbit_class_type']       = asteroid_orbit_class(individual_asteroid_data)['orbit_class_type'];
+    asteroid_info['orbit_type_description'] = asteroid_orbit_class(individual_asteroid_data)['orbit_class_description'];
+    asteroid_info['first_observation']      = asteroid_observation_dates(individual_asteroid_data)['first_observation_date'];
+    asteroid_info['last_observation']       = asteroid_observation_dates(individual_asteroid_data)['last_observation_date'];
 
     return asteroid_info;
 
-def get_info(near_earth_object, dates):
+def get_neo_info(near_earth_object, dates):
     """Get a list of informations about each Near Earth Object"""
 
-    infoNEO = defaultdict(list)
+    neo_info = defaultdict(list)
     for element_key in range(len(dates)):
         for object_type in range(len(near_earth_object[dates[element_key]])):
-            infoNEO[len(infoNEO)+1] = struct(near_earth_object, dates, element_key, object_type);
+            neo_info[len(neo_info)+1] = struct(near_earth_object, dates, element_key, object_type);
                 
-    return infoNEO;
+    return neo_info;
 
 def asteroid_orbiting_bodies(individual_asteroid_data):
     """Get each asteroid orbiting bodies"""
@@ -57,7 +57,7 @@ def asteroid_orbiting_bodies(individual_asteroid_data):
 
     return passing_by_list;
 
-def asteroid_orbiting_type(individual_asteroid_data):
+def asteroid_orbit_class(individual_asteroid_data):
     """Get each asteroid orbiting class"""
     
     orbit_class = individual_asteroid_data['orbital_data']['orbit_class'];
@@ -92,12 +92,12 @@ def print_neo_info(object_info):
             print(f"\t\t*Miles/h -> {object_info[asteroid]['velocity_miles']}")
             print(f"\t-All Orbiting Bodies -> {object_info[asteroid]['orbiting_bodies']}")
             print(f"\t-Orbit Class Type:")
-            print(f"\t\t*{object_info[asteroid]['orbiting_class_type']} -> {object_info[asteroid]['orbiting_type_description']}")
-    
+            print(f"\t\t*{object_info[asteroid]['orbit_class_type']} -> {object_info[asteroid]['orbit_type_description']}")
+                            
 def start(BASE_URL, API_KEY, start_date, end_date):
     """Start the program"""
 
-    data = get_data(BASE_URL, API_KEY, start_date, end_date);
+    data = get_api_data(BASE_URL, API_KEY, start_date, end_date);
     if data:
         num_of_object       = data["element_count"];
         near_earth_object   = data["near_earth_objects"];
@@ -106,9 +106,9 @@ def start(BASE_URL, API_KEY, start_date, end_date):
         for i in range(int(start_date[-2:]), int(end_date[-2:])+1):
             dates.append(start_date[:-2]+"%02d" % int(str(i)));
 
-        object_info = get_info(near_earth_object, dates);
+        object_info = get_neo_info(near_earth_object, dates);
         print_neo_info(object_info);
-        
+
         print("\nNumber of Near Earth Object Detected ->", num_of_object);
 
     else:
@@ -118,19 +118,15 @@ def main():
     """Program Interfact"""
 
     os.system("cls || clear");
-    print(
-        """
+    print("""
         NASA ~ ASTEROIDS NeoWs 
     (Near Earth Object Web Service)
-        """
-    )
+        """)
 
     timelapse   = input("ENTER TIMELAPSE -> [0 - 7] ");
     timelapse   = int(timelapse) if timelapse.isnumeric() else 0
-    
     end_date    = (datetime.today()).strftime("%Y-%m-%d");
     start_date  = (datetime.today() - timedelta(days=timelapse)).strftime("%Y-%m-%d")
-    
     BASE_URL    = "https://api.nasa.gov/neo/rest/v1/feed?";
     API_KEY     = ""; # --- your key API here --- #
 
