1e4c924e7d46494971174b5a858c9c0969b0109d
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-08-20 16:36:16 -0700

    Update hazardous display

--- neo.py
+++ neo.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
 from requests import get
-from datetime import datetime
 import os
 
 
@@ -15,29 +14,30 @@ def get_api_data(BASE_URL, API_KEY, start_date, end_date):
 
 def get_asteroid_info(near_earth_object, dates, element_key, object_type):
     """Get Asteroid Info Structured into a Dictionary"""
-    
-    individual_asteroid_data        = get(near_earth_object[dates[element_key]][object_type]['links']['self']).json()
-    asteroid_close_approach_data    = near_earth_object[dates[element_key]][object_type]['close_approach_data'][0]
-    asteroid        = near_earth_object[dates[element_key]][object_type]
-    asteroid_info   = defaultdict(list)
 
-    asteroid_observation_dates = get_asteroid_observation_dates(individual_asteroid_data);
-    asteroid_orbit_class = get_asteroid_orbit_class(individual_asteroid_data);
+    asteroid_info   = defaultdict(list)
+    asteroid        = near_earth_object[dates[element_key]][object_type]
+    
+    asteroid_close_approach_data    = near_earth_object[dates[element_key]][object_type]['close_approach_data'][0]
+    individual_asteroid_data        = get(near_earth_object[dates[element_key]][object_type]['links']['self']).json()
+    asteroid_observation_dates      = get_asteroid_observation_dates(individual_asteroid_data);
+    asteroid_orbit_class            = get_asteroid_orbit_class(individual_asteroid_data);
+    asteroid_orbiting_bodies        = get_asteroid_orbiting_bodies(individual_asteroid_data);
 
     asteroid_info['id']                     = asteroid['id'];
     asteroid_info['name']                   = asteroid['name'];
-    asteroid_info['hazardous']              = asteroid['is_potentially_hazardous_asteroid'];
-    asteroid_info['close_approach_date']    = asteroid_close_approach_data['close_approach_date_full'];
-    asteroid_info['current_orbiting_body']  = asteroid_close_approach_data['orbiting_body'];
     asteroid_info['diameter_meter']         = asteroid['estimated_diameter']['meters'];
     asteroid_info['diameter_feet']          = asteroid['estimated_diameter']['feet'];
+    asteroid_info['hazardous']              = "Yes" if asteroid['is_potentially_hazardous_asteroid'] == True else "No";
+    asteroid_info['close_approach_date']    = asteroid_close_approach_data['close_approach_date_full'];
+    asteroid_info['current_orbiting_body']  = asteroid_close_approach_data['orbiting_body'];
     asteroid_info['velocity_km']            = asteroid_close_approach_data['relative_velocity']['kilometers_per_hour'];
     asteroid_info['velocity_miles']         = asteroid_close_approach_data['relative_velocity']['miles_per_hour'];
-    asteroid_info['orbiting_bodies']        = get_asteroid_orbiting_bodies(individual_asteroid_data);
     asteroid_info['orbit_class_type']       = asteroid_orbit_class['orbit_class_type'];
     asteroid_info['orbit_type_description'] = asteroid_orbit_class['orbit_class_description'];
     asteroid_info['first_observation']      = asteroid_observation_dates['first_observation_date'];
     asteroid_info['last_observation']       = asteroid_observation_dates['last_observation_date'];
+    asteroid_info['orbiting_bodies']        = asteroid_orbiting_bodies;
 
     return asteroid_info;
 
@@ -79,22 +79,23 @@ def print_neo_info(neo_info):
 
     for asteroid in neo_info:
             print(f"ASTEROID #{asteroid}")
-            print(f"\t-ID -> {neo_info[asteroid]['id']}")
-            print(f"\t-Name -> {neo_info[asteroid]['name']}")
-            print(f"\t-Observation Dates:")
+            print(f"\t- ID -> {neo_info[asteroid]['id']}")
+            print(f"\t- Name -> {neo_info[asteroid]['name']}")
+            print(f"\t- Observation Dates:")
             print(f"\t\t*First -> {neo_info[asteroid]['first_observation']}")
             print(f"\t\t*Last -> {neo_info[asteroid]['last_observation']}")
-            print(f"\t-Hazardous -> {neo_info[asteroid]['hazardous']}")
-            print(f"\t-Close Approach Date -> {neo_info[asteroid]['close_approach_date']}")
-            print(f"\t-Current Orbiting Body -> {neo_info[asteroid]['current_orbiting_body']}")
-            print(f"\t-Diametre:")
+            print(f"\t- Potentially Hazardous Asteroid -> {neo_info[asteroid]['hazardous']}")
+            print(f"\t- Close Approach Date -> {neo_info[asteroid]['close_approach_date']}")
+            print(f"\t- Current Orbiting Body -> {neo_info[asteroid]['current_orbiting_body']}")
+            print(f"\t- Diametre:")
             print(f"\t\t*Min -> {int(neo_info[asteroid]['diameter_meter']['estimated_diameter_min'])}m / {int(neo_info[asteroid]['diameter_feet']['estimated_diameter_min'])}ft")
             print(f"\t\t*Max -> {int(neo_info[asteroid]['diameter_meter']['estimated_diameter_max'])}m / {int(neo_info[asteroid]['diameter_feet']['estimated_diameter_max'])}ft")
-            print(f"\t-Relative Velocity:")
+            print(f"\t- Relative Velocity:")
             print(f"\t\t*Km/h -> {neo_info[asteroid]['velocity_km']}")
             print(f"\t\t*Miles/h -> {neo_info[asteroid]['velocity_miles']}")
-            print(f"\t-All Orbiting Bodies -> {neo_info[asteroid]['orbiting_bodies']}")
-            print(f"\t-Orbit Class Type:")
+            print("\t- All Orbiting Bodies -> ", end='')
+            print(*neo_info[asteroid]['orbiting_bodies'], sep=', ')
+            print(f"\t- Orbit Class Type:")
             print(f"\t\t*{neo_info[asteroid]['orbit_class_type']} -> {neo_info[asteroid]['orbit_type_description']}")
                             
 def fetch_and_print_asteroid_info(BASE_URL, API_KEY, start_date, end_date):
@@ -132,9 +133,9 @@ def main():
         """)
 
     timelapse   = input("ENTER TIMELAPSE -> [0 - 7] ");
-    timelapse   = int(timelapse) if timelapse.isnumeric() else 0
+    timelapse   = int(timelapse) if timelapse.isnumeric() else 0;
     end_date    = (datetime.today()).strftime("%Y-%m-%d");
-    start_date  = (datetime.today() - timedelta(days=timelapse)).strftime("%Y-%m-%d")
+    start_date  = (datetime.today() - timedelta(days=timelapse)).strftime("%Y-%m-%d");
     BASE_URL    = "https://api.nasa.gov/neo/rest/v1/feed?";
     API_KEY     = ""; # --- your key API here --- #
 
